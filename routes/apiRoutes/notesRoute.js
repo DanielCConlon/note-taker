6b2128245aac9dac7c7830967bad0083b03ba0dd
Daniel Conlon <danielcconlon@gmail.com>
2022-07-02 16:31:25 -0400

    fixed db.json file object name change, can add new notes

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -4,16 +4,26 @@ const router = require('express').Router();
 
 // helper method for getting unique ids
 const uuid = require('../../helpers/uuid');
-const { readAndAppend, readFromFile, writeToFile } = require ('../../helpers/utils');
+// const { readAndAppend, readFromFile, writeToFile } = require ('../../helpers/utils');
 
+const {
+    filterByQuery,
+    findById,
+    createNewNote,
+    validateNote
+} =  require('../../lib/notes');
 
 // generate unique ids
 
 // route for notes
 router.get('/notes', (req, res) => {
-    readFromFile("./db/db.json").then((data)=>
-    res.json(JSON.parse(data))
-    );
+    let results = notes;
+
+    if(req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+
 });
 
 // create a new note
@@ -27,44 +37,44 @@ router.post('notes', (req, res) => {
     });
 });
 
-// router.get('/notes/:id', (req, res) => {
-//     const result = findById(req.params.id, notes);
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
 
-//     if(result) {
-//         res.json(result);
-//     }
-//     else{
-//         res.send(404);
-//     }
-// });
+    if(result) {
+        res.json(result);
+    }
+    else{
+        res.send(404);
+    }
+});
 
-// router.delete('/notes/:id', (req, res) => {
-//     const result = findById(req.params.id, notes);
+router.delete('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
 
-//     if(result){
-//         var oldNotes = notes;
-//         let filteredNotes = oldNotes.filter((note) => note.id !== req.params.id);
-//         console.log(filteredNotes);
+    if(result){
+        var oldNotes = notes;
+        let filteredNotes = oldNotes.filter((note) => note.id !== req.params.id);
+        console.log(filteredNotes);
 
-//         fs.writeFileSync('./db/db.json', JSON.stringify(filteredNotes));
-//         notes = filteredNotes;
-//         res.json(filteredNotes);
-//     }
-// });
+        fs.writeFileSync('./db/db.json', JSON.stringify(filteredNotes));
+        notes = filteredNotes;
+        res.json(filteredNotes);
+    }
+});
 
-// // create a new note
-// router.post('/notes', (req, res) =>{
-//     req.body.id = uuid;
+// create a new note
+router.post('/notes', (req, res) =>{
+    req.body.id = uuid;
 
-//     // if any data is incorrect, send 400 error back
-//     if(!validateNote(req.body)) {
-//         res.status(400).send('The note is not properly formatted.')
-//     }
-//     else {
-//         const newNote = createNewNote(req.body, notes);
-//         res.json(newNote);
-//     }
-// });
+    // if any data is incorrect, send 400 error back
+    if(!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    }
+    else {
+        const newNote = createNewNote(req.body, notes);
+        res.json(newNote);
+    }
+});
 
 
 
